bfe0d373464a64e6ea1f4b18df813a19734213a3
Joe Bandenburg <joe@bandenburg.com>
2015-06-23 16:15:21 +0100

    Add jscs and jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,22 @@
 module.exports = function(grunt) {
-    grunt.registerTask('test', []);
-    grunt.registerTask('default', 'test');
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-jscs");
+
+    var files = ["Gruntfile.js", "server.js", "server/**/*.js", "test/**/*.js", "public/**/*.js"];
+
+    grunt.initConfig({
+        jshint: {
+            all: files,
+            options: {
+                jshintrc: true
+            }
+        },
+        jscs: {
+            all: files
+        }
+    });
+
+    grunt.registerTask("check", ["jshint", "jscs"]);
+    grunt.registerTask("test", ["check"]);
+    grunt.registerTask("default", "test");
 };
