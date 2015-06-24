4b7a662879af416f9ee8c5b300ed4d1eb2f79b9c
Joe Bandenburg <joe@bandenburg.com>
2015-06-24 17:39:41 +0100

    add list of registered users

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,20 @@ $.ajax({
     var div = document.createElement("div");
     div.textContent = "Logged in as " + user.name;
     document.body.appendChild(div);
+
+    $.ajax({
+        url: "/api/users"
+    }).then(function(users) {
+        var usersLabel = document.createElement("h2");
+        usersLabel.textContent = "List of registered users";
+        document.body.appendChild(usersLabel);
+        var ul = document.createElement("ul");
+        users.map(makeLi).forEach(function (li) {
+            ul.appendChild(li);
+        });
+        document.body.appendChild(ul);
+    });
+ 
 }, function() {
     $.ajax({
         url: "/api/oauth/uri"
@@ -15,3 +29,9 @@ $.ajax({
     });
     console.log('not logged in');
 });
+
+function makeLi(user) {
+    var li = document.createElement("li");
+    li.textContent = user.name;
+    return li;
+}
