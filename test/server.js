6ed6e6922a7ed4580a1c43d07afc478a4abd5a95
Matthew Dunsdon <mdunsdon@scottlogic.com>
2015-08-02 10:22:31 +0000

    Introduce tests that cover "GET /api/users"

--- test/server.js
+++ test/server.js
@@ -12,6 +12,11 @@ var testUser = {
     name: "Bob Bilson",
     avatarUrl: "http://avatar.url.com/u=test"
 };
+var testUser2 = {
+    _id: "charlie",
+    name: "Charlie Colinson",
+    avatarUrl: "http://avatar.url.com/u=charlie_colinson"
+};
 var testGithubUser = {
     login: "bob",
     name: "Bob Bilson",
@@ -30,6 +35,7 @@ describe("server", function() {
         cookieJar = request.jar();
         dbCollections = {
             users: {
+                find: sinon.stub(),
                 findOne: sinon.stub(),
                 insertOne: sinon.spy()
             }
@@ -181,6 +187,73 @@ describe("server", function() {
             authenticateUser(testUser, testToken, function() {
 
                 dbCollections.users.findOne.callsArgWith(1, {err: "Database error"}, null);
+
+                request({url: requestUrl, jar: cookieJar}, function(error, response) {
+                    assert.equal(response.statusCode, 500);
+                    done();
+                });
+            });
+        });
+    });
+    describe("GET /api/users", function() {
+        var requestUrl = baseUrl + "/api/users";
+        var allUsers;
+        beforeEach(function() {
+            allUsers = {
+                toArray: sinon.stub()
+            };
+            dbCollections.users.find.returns(allUsers);
+        });
+        it("responds with status code 401 if user not authenticated", function(done) {
+            request(requestUrl, function(error, response) {
+                assert.equal(response.statusCode, 401);
+                done();
+            });
+        });
+        it("responds with status code 401 if user has an unrecognised session token", function(done) {
+            cookieJar.setCookie(request.cookie("sessionToken=" + testExpiredToken), baseUrl);
+            request({url: requestUrl, jar: cookieJar}, function(error, response) {
+                assert.equal(response.statusCode, 401);
+                done();
+            });
+        });
+        it("responds with status code 200 if user is authenticated", function(done) {
+            authenticateUser(testUser, testToken, function() {
+                allUsers.toArray.callsArgWith(0, null, [testUser]);
+
+                request({url: requestUrl, jar: cookieJar}, function(error, response) {
+                    assert.equal(response.statusCode, 200);
+                    done();
+                });
+            });
+        });
+        it("responds with a body that is a JSON representation of the user if user is authenticated", function(done) {
+            authenticateUser(testUser, testToken, function() {
+                allUsers.toArray.callsArgWith(0, null, [
+                        testUser,
+                        testUser2
+                    ]);
+
+                request({url: requestUrl, jar: cookieJar}, function(error, response, body) {
+                    assert.deepEqual(JSON.parse(body), [
+                        {
+                            id: "bob",
+                            name: "Bob Bilson",
+                            avatarUrl: "http://avatar.url.com/u=test"
+                        },
+                        {
+                            id: "charlie",
+                            name: "Charlie Colinson",
+                            avatarUrl: "http://avatar.url.com/u=charlie_colinson"
+                        }
+                    ]);
+                    done();
+                });
+            });
+        });
+        it("responds with status code 500 if user is authenticated but missing from database", function(done) {
+            authenticateUser(testUser, testToken, function() {
+                allUsers.toArray.callsArgWith(0, {err: "Database failure"}, null);
 
                 request({url: requestUrl, jar: cookieJar}, function(error, response) {
                     assert.equal(response.statusCode, 500);
