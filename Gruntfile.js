718a67860850c043c21fc904f21523ab8a78e641
Matthew Dunsdon <mdunsdon@scottlogic.com>
2015-08-02 11:04:31 +0000

    Introduce server side code coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-jscs");
     grunt.loadNpmTasks("grunt-mocha-test");
+    grunt.loadNpmTasks("grunt-mocha-istanbul");
 
     var files = ["Gruntfile.js", "server.js", "server/**/*.js", "test/**/*.js", "public/**/*.js"];
+    var artifactsLocation = "build_artifacts";
 
     grunt.initConfig({
         jshint: {
@@ -19,10 +21,56 @@ module.exports = function(grunt) {
             test: {
                 src: ["test/**/*.js"]
             }
+        },
+        "mocha_istanbul": {
+            test: {
+                src: ["test/**/*.js"]
+            },
+            options: {
+                coverageFolder: artifactsLocation,
+                reportFormats: ["none"],
+                print: "none"
+            }
+        },
+        "istanbul_report": {
+            test: {
+
+            },
+            options: {
+                coverageFolder: artifactsLocation
+            }
+        },
+        "istanbul_check_coverage": {
+            test: {
+
+            },
+            options: {
+                coverageFolder: artifactsLocation,
+                check: true
+            }
         }
     });
 
+    grunt.registerMultiTask("istanbul_report", "Solo task for generating a report over multiple files.", function () {
+        var done = this.async();
+        var cmd = process.execPath;
+        var istanbulPath = require.resolve("istanbul/lib/cli");
+        var options = this.options({
+            coverageFolder: "coverage"
+        });
+        grunt.util.spawn({
+            cmd: cmd,
+            args: [istanbulPath, "report", "--dir=" + options.coverageFolder]
+        }, function(err) {
+            if (err) {
+                return done(err);
+            }
+            done();
+        });
+    });
+
     grunt.registerTask("check", ["jshint", "jscs"]);
-    grunt.registerTask("test", ["check", "mochaTest"]);
+    grunt.registerTask("test", ["check", "mochaTest", "mocha_istanbul", "istanbul_report",
+        "istanbul_check_coverage"]);
     grunt.registerTask("default", "test");
 };
