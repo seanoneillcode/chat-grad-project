cbb7ccbccdbe92ce8ccbced2fb37b34b9ffdbb7a
Matthew Dunsdon <mdunsdon@scottlogic.com>
2015-08-02 08:34:31 +0000

    Introduce tests that cover "GET /oauth"

--- test/server.js
+++ test/server.js
@@ -7,14 +7,35 @@ var testPort = 52684;
 var baseUrl = "http://localhost:" + testPort;
 var oauthClientId = "1234clientId";
 
+var testUser = {
+    _id: "bob",
+    name: "Bob Bilson",
+    avatarUrl: "http://avatar.url.com/u=test"
+};
+var testGithubUser = {
+    login: "bob",
+    name: "Bob Bilson",
+    avatar_url: "http://avatar.url.com/u=test"
+};
+var testToken = "123123";
+
 describe("server", function() {
     var db;
     var githubAuthoriser;
     var serverInstance;
+    var dbCollections;
     beforeEach(function() {
-        db = {
-            collection: sinon.spy()
+        dbCollections = {
+            users: {
+                findOne: sinon.stub(),
+                insertOne: sinon.spy()
+            }
         };
+        db = {
+            collection: sinon.stub()
+        };
+        db.collection.withArgs("users").returns(dbCollections.users);
+
         githubAuthoriser = {
             authorise: function() {},
             oAuthUri: "https://github.com/login/oauth/authorize?client_id=" + oauthClientId
@@ -23,6 +44,65 @@ describe("server", function() {
     });
     afterEach(function() {
         serverInstance.close();
+    });
+    describe("GET /oauth", function() {
+        var requestUrl = baseUrl + "/oauth";
+
+        it("responds with status code 400 if oAuth authorise fails", function(done) {
+            var stub = sinon.stub(githubAuthoriser, "authorise", function(req, callback) {
+                callback(null);
+            });
+
+            request(requestUrl, function(error, response) {
+                assert.equal(response.statusCode, 400);
+                done();
+            });
+        });
+        it("responds with status code 302 if oAuth authorise succeeds", function(done) {
+            var user = testGithubUser;
+            var stub = sinon.stub(githubAuthoriser, "authorise", function(req, authCallback) {
+                authCallback(user, testToken);
+            });
+
+            dbCollections.users.findOne.callsArgWith(1, null, user);
+
+            request({url: requestUrl, followRedirect: false}, function(error, response) {
+                assert.equal(response.statusCode, 302);
+                done();
+            });
+        });
+        it("responds with a redirect to '/' if oAuth authorise succeeds", function(done) {
+            var user = testGithubUser;
+            var stub = sinon.stub(githubAuthoriser, "authorise", function(req, authCallback) {
+                authCallback(user, testToken);
+            });
+
+            dbCollections.users.findOne.callsArgWith(1, null, user);
+
+            request(requestUrl, function(error, response) {
+                assert.equal(response.statusCode, 200);
+                assert.equal(response.request.uri.path, "/");
+                done();
+            });
+        });
+        it("add user to database if oAuth authorise succeeds and user id not found", function(done) {
+            var user = testGithubUser;
+            var stub = sinon.stub(githubAuthoriser, "authorise", function(req, authCallback) {
+                authCallback(user, testToken);
+            });
+
+            dbCollections.users.findOne.callsArgWith(1, null, null);
+
+            request(requestUrl, function(error, response) {
+                assert(dbCollections.users.insertOne.calledOnce);
+                assert.deepEqual(dbCollections.users.insertOne.firstCall.args[0], {
+                    _id: "bob",
+                    name: "Bob Bilson",
+                    avatarUrl: "http://avatar.url.com/u=test"
+                });
+                done();
+            });
+        });
     });
     describe("GET /api/oauth/uri", function() {
         var requestUrl = baseUrl + "/api/oauth/uri";
